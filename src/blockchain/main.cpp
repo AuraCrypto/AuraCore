4a453d1ebc5c76bf6d601ef1ecdc23823ee7a331
Aura Developer <poka.com@proton.me>
2025-01-29 16:11:53 +0330

    Update main.cpp

--- src/blockchain/main.cpp
+++ src/blockchain/main.cpp
@@ -2,17 +2,14 @@
 #include "block.h"
 
 int main() {
-    // سختی شبکه (تعداد صفرهای موردنیاز در ابتدای هش)
+
     int difficulty = 4;
 
-    // ساخت بلاک اولیه (Genesis Block)
     std::cout << "Creating Genesis Block..." << std::endl;
     Block genesisBlock(0, "0", "Genesis Block Data");
 
-    // ماین کردن بلاک اولیه
     genesisBlock.mineBlock(difficulty);
 
-    // نمایش اطلاعات بلاک اولیه
     std::cout << "Block Mined!" << std::endl;
     std::cout << "Index: " << 0 << std::endl;
     std::cout << "Hash: " << genesisBlock.getHash() << std::endl;
